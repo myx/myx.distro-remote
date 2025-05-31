b36aabfa066b31ec9aec87a76b1fc0609b595a18
Alex Kharichev <myx@myx.nz>
2025-05-31 20:25:01 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# myx.distro-remote
+
+	Tools for working with myx.distro on remote host.
+
+
+See: [distro](https://github.com/myx/myx.distro?tab=readme-ov-file#myxdistro)
+See: [distro-.local](https://github.com/myx/myx.distro-.local?tab=readme-ov-file#myxdistro-.local)
+See: [distro-system](https://github.com/myx/myx.distro-system?tab=readme-ov-file#myxdistro-system)
+See: [distro-deploy](https://github.com/myx/myx.distro-deploy?tab=readme-ov-file#myxdistro-deploy)
+See: [distro-source](https://github.com/myx/myx.distro-source?tab=readme-ov-file#myxdistro-source)
+See: [distro-remote](https://github.com/myx/myx.distro-source?tab=readme-ov-file#myxdistro-remote)
---
+++ sh-scripts/DistroRemoteConsole.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+if [ -z "$MMDAPP" ] ; then
+	set -e
+	export MMDAPP="$( cd $(dirname "$0")/../../../.. ; pwd )"
+	echo "$0: Working in: $MMDAPP"  >&2
+	[ -d "$MMDAPP/source" ] || ( echo "ERROR: expecting 'source' directory." >&2 && exit 1 )
+fi
+
+
+
+DistroRemoteConsole(){
+	local MDSC_CMD='DistroRemoteConsole'
+	[ -z "$MDSC_DETAIL" ] || echo "> $MDSC_CMD $@" >&2
+
+	set -e
+
+	local consoleModeOverride
+
+	while true ; do
+		case "$1" in
+			--source|--deploy|--remote|--manage)
+				consoleModeOverride="${1:2}"
+				shift
+			;;
+			--interactive)
+				shift
+
+				echo "Not yet!"
+				exit 1
+
+				return 0
+			;;
+			'')
+				echo "ERROR: $MDSC_CMD: more arguments expected" >&2
+				set +e ; return 1
+			;;
+			*)
+				echo "ERROR: $MDSC_CMD: invalid option: $1" >&2
+				set +e ; return 1
+			;;
+		esac
+	done
+}
+
+
+case "$0" in
+	*/myx/myx.distro-image/sh-scripts/DistroRemoteConsole.sh)
+
+		if [ -z "$1" ] || [ "$1" = "--help" ] ; then
+			echo "syntax: DistroRemoteConsole.sh [--{source|deploy|remote|manage}] [<project-name-glob>]" >&2
+			echo "syntax: DistroRemoteConsole.sh [--{source|deploy|remote|manage}] --select-remote [<remote-name-glob>]" >&2
+			echo "syntax: DistroRemoteConsole.sh --interactive" >&2
+			echo "syntax: DistroRemoteConsole.sh [--help]" >&2
+			if [ "$1" = "--help" ] ; then
+				cat "$MMDAPP/source/myx/myx.distro-image/sh-lib/HelpDistroRemoteConsole.text" >&2
+			fi
+			exit 1
+		fi
+		
+		DistroRemoteConsole "$@"
+	;;
+esac
