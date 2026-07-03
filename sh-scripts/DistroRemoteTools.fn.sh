f1cd1b438512694137868e1a958ca71cdd246e80
Alex Kharichev <myx@myx.nz>
2026-07-03 13:37:22 +0400

    * minor: DistroRemoteTools refactoring

--- sh-scripts/DistroRemoteTools.fn.sh
+++ sh-scripts/DistroRemoteTools.fn.sh
@@ -12,35 +12,38 @@ if [ -z "$MMDAPP" ] ; then
 	[ -d "$MMDAPP/.local" ] || ( echo "⛔ ERROR: expecting '.local' directory." >&2 && exit 1 )
 fi
 
+: "${MDLT_ORIGIN:=$MMDAPP/.local}"
+
 DistroRemoteTools(){
 	local MDSC_CMD='DistroRemoteTools'
-	[ -z "$MDSC_DETAIL" ] || echo "> $MDSC_CMD $(printf '%q ' "$@")" >&2
-	local includeRemoteConfig="$MMDAPP/.local/myx/myx.distro-remote/sh-lib/DistroRemoteToolsConfig.include"
-	[ -f "$includeRemoteConfig" ] || includeRemoteConfig="$MMDAPP/source/myx/myx.distro-remote/sh-lib/DistroRemoteToolsConfig.include"
+	[ -z "$MDSC_DETAIL" ] || echo "> $MDSC_CMD" $MDSC_NO_CACHE $MDSC_NO_INDEX "$@" >&2
 
 	set -e
 
+	. "$MDLT_ORIGIN/myx/myx.distro-system/sh-lib/SystemContext.UseStandardOptions.include"
+
 	case "$1" in
 		--make-*)
-			. "$MMDAPP/.local/myx/myx.distro-remote/sh-lib/RemoteTools.Make.include"
-			set +e ; return 1
+			. "$MDLT_ORIGIN/myx/myx.distro-remote/sh-lib/RemoteTools.Make.include"
+			return 0
 		;;
-		--system-config-option|--custom-config-option)
-			. "$includeRemoteConfig"
-			set +e ; return 1
+		--*-config-option)
+			. "$MDLT_ORIGIN/myx/myx.distro-.local/sh-lib/LocalTools.Config.include"
+			return 0
 		;;
 		--upgrade-remote-tools)
 			shift
-			bash "$MMDAPP/.local/myx/myx.distro-.local/sh-scripts/DistroLocalTools.fn.sh" --install-distro-remote
+			bash "$MDLT_ORIGIN/myx/myx.distro-.local/sh-scripts/DistroLocalTools.fn.sh" --install-distro-remote
 			return 0
 		;;
 		''|--help|--help-syntax)
 			echo "📘 syntax: DistroRemoteTools.fn.sh <option>" >&2
+			echo "📘 syntax: DistroRemoteTools.fn.sh --upgrade-remote-tools" >&2
 			echo "📘 syntax: DistroRemoteTools.fn.sh [--help]" >&2
 			if [ "$1" = "--help" ] ; then
-				cat "$MMDAPP/.local/myx/myx.distro-remote/sh-lib/HelpDistroRemoteTools.text" >&2
+				cat "$MDLT_ORIGIN/myx/myx.distro-remote/sh-lib/HelpDistroRemoteTools.text" >&2
 			fi
-			set +e ; return 1
+			return 0
 		;;
 		*)
 			echo "⛔ ERROR: $MDSC_CMD: invalid option: $1" >&2
@@ -51,12 +54,18 @@ DistroRemoteTools(){
 
 case "$0" in
 	*/myx/myx.distro-remote/sh-scripts/DistroRemoteTools.fn.sh)
+		set -e
+
+		if [ -z "$MDLT_ORIGIN" ] || ! type DistroRemoteContext >/dev/null 2>&1 ; then
+			. "${MDLT_ORIGIN:=$MMDAPP/.local}/myx/myx.distro-remote/sh-lib/RemoteContext.include"
+		fi
+		DistroRemoteContext --run-from-detect
 
 		if [ -z "$1" ] || [ "$1" = "--help" ] ; then
-			echo "syntax: DistroRemoteTools.fn.sh --upgrade-remote-tools" >&2
+			DistroRemoteTools "${1:-"--help-syntax"}"
+			exit 1
 		fi
 
-		set -e
 		DistroRemoteTools "$@"
 	;;
 esac
